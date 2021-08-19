fe9a9471ef9450510c021fc71e9c106f98b642e7
rosy871 <yalameju@gmail.com>
2021-08-19 10:21:53 +0200

    add dataAnnotations

--- Garage2/Models/ParkedVehicle.cs
+++ Garage2/Models/ParkedVehicle.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,21 +8,31 @@ namespace Garage2.Models
 {
     public class ParkedVehicle
     {
+        
+        [Required]
+        [StringLength(30)]
         // Car, Motorcycle etc. 
         public string VehicleType { get; set; }
 
+        [Required]
         public string LicensePlate { get; set; }
+
 
         public string Color { get; set; }
 
+        [Required]
         // Volvo, Audi etc.
         public String Make { get; set; }
 
         // V90, A6 etc.
         public string Model { get; set; }
 
+        [Required]
+        [Range(1, 6)]
         public int Wheels { get; set; }
 
+        [DataType(DataType.Time)]
+        [Display(Name = "Arrived Time")]
         public DateTime ArrivalTime { get; set; }
 
         // Parked or "Unparked / Removed from Garage" 
