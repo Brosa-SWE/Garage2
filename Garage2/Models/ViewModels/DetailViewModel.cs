767f330af601b5fda409820490c7166aa8bc77a5
rosy871 <yalameju@gmail.com>
2021-08-20 13:00:15 +0200

    added detailviewmodel

---
+++ Garage2/Models/ViewModels/DetailViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Garage2.Models.ViewModels
+{
+    public class DetailViewModel
+    {
+		public int Id { get; set; }
+		public VehicleType VehicleType { get; set; }
+		public string LicensePlate { get; set; }
+		public VehicleColor Color { get; set; }
+		public String Make { get; set; }
+		public string Model { get; set; }
+		public int Wheels { get; set; }
+		public DateTime ArrivalTime { get; set; }
+		[Display(Name = "Parked Time")]
+		public TimeSpan ParkedTime { get; set; }
+	
+	}
+}
