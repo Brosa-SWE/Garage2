a64e9dd49857844ebe5b29a1cd2e413c3695246a
tom-sharp <dink521g@gmail.com>
2021-08-24 11:21:31 +0200

    Created Initial Slotmanager

---
+++ Garage2/Migrations/20210824083905_AddedSlotManager.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Garage2.Migrations
+{
+    public partial class AddedSlotManager : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "GarageSlot",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SlotId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    InUse = table.Column<bool>(type: "bit", nullable: false),
+                    BlockId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GarageSlot", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GarageSlot");
+        }
+    }
+}
