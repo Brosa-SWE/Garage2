231bd2f2d1388f71f86d33daf67b34218916f914
Brosa-SWE <per.gref@gmail.com>
2021-08-25 11:14:30 +0200

    Added Statistics Presentation Page

---
+++ Garage2/Migrations/20210825071326_FixPG.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Garage2.Migrations
+{
+    public partial class FixPG : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GarageSlot");
+
+            migrationBuilder.AddColumn<string>(
+                name: "SlotsInUse",
+                table: "ParkedVehicle",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SlotsInUse",
+                table: "ParkedVehicle");
+
+            migrationBuilder.CreateTable(
+                name: "GarageSlot",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BlockId = table.Column<int>(type: "int", nullable: false),
+                    InUse = table.Column<int>(type: "int", nullable: false),
+                    SlotId = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GarageSlot", x => x.Id);
+                });
+        }
+    }
+}
